7f353201d4631ed6043b2ac3b967207e27af14da
7BLIXgh <c.wetter@web.de>
2025-04-20 00:57:44 +0200

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>🎉 Branchenprofi ist live!</h1>
+      <p><a href="/save">➡️ Zur Speichern-Oberfläche</a></p>
+    </div>
+  );
+}
