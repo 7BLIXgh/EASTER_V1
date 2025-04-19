a9f3c2e3bbb60b4fc1c7c2bdbfc67c232f2bc765
7BLIXgh <c.wetter@web.de>
2025-04-20 01:01:45 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,11 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/save');
+  }, []);
   return (
     <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
       <h1>🎉 Branchenprofi ist live!</h1>
